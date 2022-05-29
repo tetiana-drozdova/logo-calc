32d432e61d36806c092f942688205ff28bceac4a
tetiana-drozdova <gravvi.dreams@gmail.com>
2022-05-29 18:53:11 +0300

    Markup

--- src/Pages/Define.js
+++ src/Pages/Define.js
@@ -1,7 +1,27 @@
 export default function Define() {
-  return (
-    <div>
-      <h2>Define</h2>
-    </div>
-  );
+	return (
+		<div>
+			
+			<div>
+				<form>
+					<label>
+						Height:&nbsp;
+						<input type="number" name="height" />
+					</label>
+					<br />
+					<label>
+						Width:&nbsp;
+						<input type="number" name="width" />
+					</label>
+					<br />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+
+			<div>
+				<p>Drop a file</p>
+			</div>
+
+		</div>
+	);
 }
